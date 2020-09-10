707ddfd933ee44174c507c9fae57d0b05937755a
Ethan <eleetyson@gmail.com>
2020-09-10 10:58:07 -0400

    add actions and views for show, new

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,4 +2,15 @@ class PartiesController < ApplicationController
     def index
         @parties = Party.all
     end
+
+    def show
+        @party = Party.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+      
+    end
 end
